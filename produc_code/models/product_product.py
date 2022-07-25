db536e1d71f78958b1f5a28d50cd14ce37d6eafc
Moises Navarro <moisalejandro@gmail.com>
2022-07-25 12:50:30 -0500

    [FIX] label

--- produc_code/models/product_product.py
+++ produc_code/models/product_product.py
@@ -5,4 +5,4 @@ class Product(models.Model):
     _inherit = "product.product"
 
     id_amazon = fields.Char(string="ID Amazon")
-    id_ebay = fields.Char(string="ID Amazon")
+    id_ebay = fields.Char(string="ID Ebay")
