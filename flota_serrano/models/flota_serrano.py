2438856411d0e308b62817676dafa5e4c70fec87
Fernando Rodriguez <fernando@MacBook-Pro-de-Fernando.local>
2022-08-04 21:33:32 -0500

    Tabulado
    Tab

--- flota_serrano/models/flota_serrano.py
+++ flota_serrano/models/flota_serrano.py
@@ -1,23 +1,11 @@
-from odoo import fields, models,api
+from odoo import fields, models, api
 class FlotaSerrano(models.Model):
-
     _inherit = "sale.order"
-    marca = fields.Char(
-        string="Marca",
-    )
-    modelo = fields.Char(
-        string="Modelo",
-    )
-    km = fields.Float(
-        string="Kilometraje"
-    )
-    rendimiento = fields.Float(
-        default=2.2,
-        string="Rendimiento"
-    )
-    disel = fields.Float(
-        compute='_compute_disel',
-    )
+    marca = fields.Char(string="Marca",)
+    modelo = fields.Char(string="Modelo",)
+    km = fields.Float(string="Kilometraje")
+    rendimiento = fields.Float(default=2.2,string="Rendimiento",)
+    disel = fields.Float(compute='_compute_disel',)
 
     @api.depends("km", "rendimiento")
     def _compute_disel(self):
