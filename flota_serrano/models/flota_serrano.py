d878ac8a1e1a7dcc14a7ffc9dc1502393bc9a133
Fernando Rodriguez <fernando@MacBook-Pro-de-Fernando.local>
2022-08-05 10:17:36 -0500

    Flota_2022
    Flota_2022

--- flota_serrano/models/flota_serrano.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models, api
-class FlotaSerrano(models.Model):
-    _inherit = "sale.order"
-    marca = fields.Char(string="Marca",)
-    modelo = fields.Char(string="Modelo",)
-    km = fields.Float(string="Kilometraje")
-    rendimiento = fields.Float(default=2.2,string="Rendimiento",)
